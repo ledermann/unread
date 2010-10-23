6c889764f1dad1281346da5b9a064d9d7a61ec82
Georg Ledermann <mail@georg-ledermann.de>
2010-10-23 16:26:26 +0200

    Tests with two emails

--- test/unread_test.rb
+++ test/unread_test.rb
@@ -5,7 +5,8 @@ class UnreadTest < ActiveSupport::TestCase
     @user = User.create! :name => 'David'
     @other_user = User.create :name => 'Matz'
     wait
-    @email = Email.create!
+    @email1 = Email.create!
+    @email2 = Email.create!
   end
   
   def teardown
@@ -15,60 +16,59 @@ class UnreadTest < ActiveSupport::TestCase
   end
   
   def test_schema_has_loaded_correctly
-    assert_equal [@email], Email.all
+    assert_equal [@email1, @email2], Email.all
   end
 
   def test_scope
-    assert_equal [@email], Email.unread_by(@user)
-    assert_equal [@email], Email.unread_by(@other_user)
+    assert_equal [@email1, @email2], Email.unread_by(@user)
+    assert_equal [@email1, @email2], Email.unread_by(@other_user)
     
-    assert_equal 1, Email.unread_by(@user).count
-    assert_equal 1, Email.unread_by(@other_user).count
+    assert_equal 2, Email.unread_by(@user).count
+    assert_equal 2, Email.unread_by(@other_user).count
   end
   
   def test_scope_after_reset
-    @email.mark_as_read! :for => @user
-    second_mail = Email.create! :updated_at => Time.now
+    @email1.mark_as_read! :for => @user
     
-    assert_equal [second_mail], Email.unread_by(@user)
+    assert_equal [@email2], Email.unread_by(@user)
     assert_equal 1, Email.unread_by(@user).count
   end
 
   def test_unread_by
-    assert_equal true, @email.unread?(@user)
-    assert_equal true, @email.unread?(@other_user)
+    assert_equal true, @email1.unread?(@user)
+    assert_equal true, @email1.unread?(@other_user)
   end
   
   def test_unread_after_update
-    @email.mark_as_read! :for => @user
+    @email1.mark_as_read! :for => @user
     wait
-    @email.update_attributes! :subject => 'changed'
+    @email1.update_attributes! :subject => 'changed'
 
-    assert_equal true, @email.unread?(@user)
+    assert_equal true, @email1.unread?(@user)
   end
   
   def test_mark_as_read
-    @email.mark_as_read! :for => @user
+    @email1.mark_as_read! :for => @user
     
-    assert_equal false, @email.unread?(@user)
-    assert_equal [], Email.unread_by(@user)
+    assert_equal false, @email1.unread?(@user)
+    assert_equal [@email2], Email.unread_by(@user)
     
-    assert_equal true, @email.unread?(@other_user)
-    assert_equal [@email], Email.unread_by(@other_user)
+    assert_equal true, @email1.unread?(@other_user)
+    assert_equal [@email1, @email2], Email.unread_by(@other_user)
     
     assert_equal 1, @user.read_marks.single.count
-    assert_equal @email, @user.read_marks.single.first.readable
+    assert_equal @email1, @user.read_marks.single.first.readable
   end
   
   def test_mark_as_read_multiple
     other_mail = Email.create! :updated_at => Time.now
     
-    assert_equal true, @email.unread?(@user)
+    assert_equal true, @email1.unread?(@user)
     assert_equal true, other_mail.unread?(@user)
     
-    Email.mark_as_read! [ @email.id, other_mail.id ], :for => @user
+    Email.mark_as_read! [ @email1.id, other_mail.id ], :for => @user
     
-    assert_equal false, @email.unread?(@user)
+    assert_equal false, @email1.unread?(@user)
     assert_equal false, other_mail.unread?(@user)
   end
   
@@ -76,14 +76,14 @@ class UnreadTest < ActiveSupport::TestCase
     wait
     
     Email.mark_as_read! :all, :for => @user
-    @email.mark_as_read! :for => @user
+    @email1.mark_as_read! :for => @user
     
     assert_equal [], @user.read_marks.single
   end
   
   def test_mark_as_read_twice
-    @email.mark_as_read! :for => @user
-    @email.mark_as_read! :for => @user
+    @email1.mark_as_read! :for => @user
+    @email1.mark_as_read! :for => @user
     
     assert_equal 1, @user.read_marks.single.count
   end
@@ -98,6 +98,8 @@ class UnreadTest < ActiveSupport::TestCase
   end
   
   def test_cleanup_read_marks
+    
+    
     Email.cleanup_read_marks!
   end
   
