cd3e790d02c43edfa937d4637df7953eefbf5f6b
Georg Ledermann <mail@georg-ledermann.de>
2013-02-17 09:41:06 +0100

    Tests: Fixed class loading issue with Rails 3.0

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require 'active_support'
 require 'active_support/test_case'
 require 'active_record'
 require 'timecop'
-require 'unread'
 
 configs = YAML.load_file(File.dirname(__FILE__) + '/database.yml')
 ActiveRecord::Base.configurations = configs
@@ -12,6 +11,8 @@ db_name = ENV['DB'] || 'sqlite'
 ActiveRecord::Base.establish_connection(db_name)
 ActiveRecord::Migration.verbose = false
 load(File.dirname(__FILE__) + "/schema.rb")
+
+require 'unread'
 
 class Reader < ActiveRecord::Base
   acts_as_reader
