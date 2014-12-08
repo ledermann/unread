148d97d3875346ee0ed7adbe86a809b99744db9d
Miguel Cervera <miguelcerverac@gmail.com>
2014-12-07 19:13:49 -0800

    fixed query count by setting the read_mark_global

--- lib/unread/reader_scopes.rb
+++ lib/unread/reader_scopes.rb
@@ -13,15 +13,17 @@ module Unread
       end
 
       def have_not_read(readable)
-        result = join_read_marks(readable)
-        result = result.where "read_marks.id IS NULL"
+        result = join_read_marks(readable).where "read_marks.id IS NULL"
         result
       end
 
       def have_read(readable)
-        result = join_read_marks(readable)
-        result = result.where('read_marks.id IS NOT NULL')
+        result = join_read_marks(readable).where('read_marks.id IS NOT NULL')
         result
+      end
+
+      def with_read_marks_for(readable)
+        join_read_marks(readable).select("#{table_name}.*, read_marks.id AS read_mark_id")
       end
     end
   end
