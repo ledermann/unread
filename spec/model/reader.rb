3f075cb8ac346fcb1065a18f3fd91cb1b1fe8ff6
Georg Ledermann <mail@georg-ledermann.de>
2015-02-03 10:09:20 +0100

    Specs: Don't use `where.not`
    `where.not` isn’t available in Rails < 4

--- spec/model/reader.rb
+++ spec/model/reader.rb
@@ -1,8 +1,8 @@
 class Reader < ActiveRecord::Base
   self.primary_key = 'number'
 
-  scope :not_foo, -> { where.not(:name => 'foo') }
-  scope :not_bar, -> { where.not(:name => 'bar') }
+  scope :not_foo, -> { where('name <> "foo"') }
+  scope :not_bar, -> { where('name <> "bar"') }
 
   acts_as_reader :scopes => [:not_foo, :not_bar]
 end
