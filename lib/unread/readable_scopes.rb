0589eedbf0938020792bfb69269257e945337774
Georg Ledermann <mail@georg-ledermann.de>
2015-05-27 14:06:38 +0200

    Use quote_column_name for building SQL

--- lib/unread/readable_scopes.rb
+++ lib/unread/readable_scopes.rb
@@ -8,14 +8,15 @@ module Unread
                 ON read_marks.readable_type  = '#{base_class.name}'
                AND read_marks.readable_id    = #{quoted_table_name}.#{quoted_primary_key}
                AND read_marks.user_id        = #{user.id}
-               AND read_marks.timestamp     >= #{quoted_table_name}.#{readable_options[:on]}"
+               AND read_marks.timestamp     >= #{quoted_table_name}.#{connection.quote_column_name(readable_options[:on])}"
       end
 
       def unread_by(user)
         result = join_read_marks(user)
 
         if global_time_stamp = user.read_mark_global(self).try(:timestamp)
-          result = result.where("read_marks.id IS NULL AND #{quoted_table_name}.#{readable_options[:on]} > ?", global_time_stamp)
+          result = result.where("read_marks.id IS NULL
+                                 AND #{quoted_table_name}.#{connection.quote_column_name(readable_options[:on])} > ?", global_time_stamp)
         else
           result = result.where('read_marks.id IS NULL')
         end
@@ -27,7 +28,8 @@ module Unread
         result = join_read_marks(user)
 
         if global_time_stamp = user.read_mark_global(self).try(:timestamp)
-          result = result.where("read_marks.id IS NOT NULL OR #{quoted_table_name}.#{readable_options[:on]} <= ?", global_time_stamp)
+          result = result.where("read_marks.id IS NOT NULL
+                                 OR #{quoted_table_name}.#{connection.quote_column_name(readable_options[:on])} <= ?", global_time_stamp)
         else
           result = result.where('read_marks.id IS NOT NULL')
         end
