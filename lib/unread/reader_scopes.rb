567cf74e1dd3849bf98669fa2df5f06da5947380
Georg Ledermann <mail@georg-ledermann.de>
2015-05-27 06:24:37 +0200

    Code formatting

--- lib/unread/reader_scopes.rb
+++ lib/unread/reader_scopes.rb
@@ -4,7 +4,8 @@ module Unread
       def join_read_marks(readable)
         assert_readable(readable)
 
-        joins "LEFT JOIN #{ReadMark.table_name} as read_marks ON read_marks.readable_type = '#{readable.class.base_class.name}'
+        joins "LEFT JOIN #{ReadMark.table_name} AS read_marks
+                ON read_marks.readable_type  = '#{readable.class.base_class.name}'
                AND (read_marks.readable_id   = #{readable.id} OR read_marks.readable_id IS NULL)
                AND read_marks.user_id        = #{table_name}.#{primary_key}
                AND read_marks.timestamp     >= '#{readable.send(readable.class.readable_options[:on]).to_s(:db)}'"
