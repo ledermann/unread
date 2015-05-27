4968b3b8b04619eee91128d948bcd774132af44c
Georg Ledermann <mail@georg-ledermann.de>
2015-05-27 06:17:32 +0200

    Scopes: Simplified building JOIN statement
    Simple #id works for non-default primary key name, too

--- lib/unread/reader_scopes.rb
+++ lib/unread/reader_scopes.rb
@@ -5,7 +5,7 @@ module Unread
         assert_readable(readable)
 
         joins "LEFT JOIN #{ReadMark.table_name} as read_marks ON read_marks.readable_type = '#{readable.class.base_class.name}'
-               AND (read_marks.readable_id   = #{readable.send(readable.class.primary_key)} OR read_marks.readable_id IS NULL)
+               AND (read_marks.readable_id   = #{readable.id} OR read_marks.readable_id IS NULL)
                AND read_marks.user_id        = #{table_name}.#{primary_key}
                AND read_marks.timestamp     >= '#{readable.send(readable.class.readable_options[:on]).to_s(:db)}'"
       end
@@ -21,7 +21,7 @@ module Unread
       def with_read_marks_for(readable)
         join_read_marks(readable).select("#{table_name}.*, read_marks.id AS read_mark_id,
                                          '#{readable.class.base_class.name}' AS read_mark_readable_type,
-                                          #{readable.try(readable.class.primary_key)} AS read_mark_readable_id")
+                                          #{readable.id} AS read_mark_readable_id")
       end
     end
   end
