f3ae96f7e8c7ce968b07e8ac08ac2ce19cee058a
Georg Ledermann <mail@georg-ledermann.de>
2015-11-05 15:56:30 +0100

    Fix SQL for PostgreSQL

--- spec/app/models/reader.rb
+++ spec/app/models/reader.rb
@@ -1,8 +1,8 @@
 class Reader < ActiveRecord::Base
   self.primary_key = 'number'
 
-  scope :not_foo, -> { where('name <> "foo"') }
-  scope :not_bar, -> { where('name <> "bar"') }
+  scope :not_foo, -> { where("name <> 'foo'") }
+  scope :not_bar, -> { where("name <> 'bar'") }
 
   acts_as_reader
 
