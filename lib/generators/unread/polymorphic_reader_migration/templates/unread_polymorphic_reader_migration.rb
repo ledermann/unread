a2cbee9b2b9a07b42ac85f25d3d0f14134424e86
Georg Ledermann <mail@georg-ledermann.de>
2016-08-22 12:35:57 +0200

    Use unique index in upgrade migration, too
    Belongs to #78

--- lib/generators/unread/polymorphic_reader_migration/templates/unread_polymorphic_reader_migration.rb
+++ lib/generators/unread/polymorphic_reader_migration/templates/unread_polymorphic_reader_migration.rb
@@ -4,7 +4,7 @@ class UnreadPolymorphicReaderMigration < ActiveRecord::Migration
     rename_column :read_marks, :user_id, :reader_id
     add_column :read_marks, :reader_type, :string
     execute "update read_marks set reader_type = 'User'"
-    add_index :read_marks, [:reader_id, :reader_type, :readable_type, :readable_id], name: 'read_marks_reader_readable_index'
+    add_index :read_marks, [:reader_id, :reader_type, :readable_type, :readable_id], name: 'read_marks_reader_readable_index', unique: true
   end
 
   def self.down
