b3e257b6c450fe9256e99a3949dfa5a382a0e5fd
Fotos Georgiadis <gfotos@gmail.com>
2019-08-25 01:30:39 -0700

    Quote the readable.id in Unread::Reader::Scopes (#108)
    * Quote the readable.id in Unread::Reader::Scopes
    
    Fixes an SQL syntax error when the readable.id is a UUID.
    
    * Add parantheses around quote_bound_value argument in Unread::Reader::Scopes

--- lib/unread/reader_scopes.rb
+++ lib/unread/reader_scopes.rb
@@ -12,7 +12,7 @@ module Unread
 
         joins "LEFT JOIN #{ReadMark.quoted_table_name}
                 ON #{ReadMark.quoted_table_name}.readable_type  = '#{readable.class.readable_parent.name}'
-               AND (#{ReadMark.quoted_table_name}.readable_id   = #{readable.id} OR #{ReadMark.quoted_table_name}.readable_id IS NULL)
+               AND (#{ReadMark.quoted_table_name}.readable_id   = #{quote_bound_value(readable.id)} OR #{ReadMark.quoted_table_name}.readable_id IS NULL)
                AND #{ReadMark.quoted_table_name}.reader_id      = #{quoted_table_name}.#{quoted_primary_key}
                AND #{ReadMark.quoted_table_name}.reader_type    = '#{connection.quote_string base_class.name}'
                AND #{ReadMark.quoted_table_name}.timestamp     >= '#{connection.quoted_date readable.send(readable.class.readable_options[:on])}'"
@@ -31,8 +31,8 @@ module Unread
 
         join_read_marks(readable).select("#{quoted_table_name}.*,
                                           #{ReadMark.quoted_table_name}.id AS read_mark_id,
-                                          #{quote_bound_value readable.class.name}#{postgresql_string_cast} AS read_mark_readable_type,
-                                          #{readable.id} AS read_mark_readable_id")
+                                          #{quote_bound_value(readable.class.name)}#{postgresql_string_cast} AS read_mark_readable_type,
+                                          #{quote_bound_value(readable.id)} AS read_mark_readable_id")
       end
     end
   end
