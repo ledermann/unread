25a9cfd888e07acc7441d53868c6f10ae74c93ca
fatkodima <fatkodima123@gmail.com>
2022-11-26 23:52:49 +0200

    Do not prematurely load `ActiveRecord::Base`

--- lib/unread.rb
+++ lib/unread.rb
@@ -1,7 +1,6 @@
 require 'active_record'
 
 require 'unread/base'
-require 'unread/read_mark'
 require 'unread/readable'
 require 'unread/reader'
 require 'unread/readable_scopes'
@@ -9,10 +8,14 @@ require 'unread/reader_scopes'
 require 'unread/garbage_collector'
 require 'unread/version'
 
-ActiveRecord::Base.send :include, Unread
-
 Unread::MIGRATION_BASE_CLASS = if ActiveRecord::VERSION::MAJOR >= 5
   ActiveRecord::Migration[5.0]
 else
   ActiveRecord::Migration
+end
+
+ActiveSupport.on_load(:active_record) do
+  require 'unread/read_mark'
+
+  include Unread
 end
